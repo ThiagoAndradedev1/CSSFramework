f1d7040abc0dae92d954980e6a5c8a933782ca5e
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-19 19:51:45 -0300

    novas modificações no component Button

--- core/Button.js
+++ core/Button.js
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
   color: palevioletred;
-  margin: 0.5em 1em;
+  font-size: 1em;
+  margin: 1em;
   padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
-  ${props => props.primary && css`
-    background: palevioletred;
-    color: white;
-  `}
+// A new component based on Button, but with some override styles
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
 `;
 
 export default Button;
