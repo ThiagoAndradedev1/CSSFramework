f0a065d9a4bf1e772b0eaa657e08923c9a0b1e15
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-19 20:27:23 -0300

    Novas alterações no componente Button

--- core/Button.js
+++ core/Button.js
@@ -8,10 +8,10 @@ const Button = styled.button`
   margin: 0.5em 1em;
   padding: 0.25em 1em;
 
-  ${props => props.primary && css`
-    background: palevioletred;
-    color: white;
-  `}
+  ${props => props.primary `
+  background: palevioletred;
+  color: white;
+`}
 `;
 
 export default Button;
