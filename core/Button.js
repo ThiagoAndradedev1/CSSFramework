e9cc47a89029af70d08d2bf6c0722f0794bec202
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-19 20:12:05 -0300

    Alterando cor do botão

--- core/Button.js
+++ core/Button.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  color: palevioletred;
+  color: yellow;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid blue;
   border-radius: 3px;
 `;
 
