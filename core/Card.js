8bc6ffabe4b98bbd900edd076b4b8eeeffa3451d
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-21 15:43:39 -0300

    Alterando detalhes do componente card

--- core/Card.js
+++ core/Card.js
@@ -1,13 +1,9 @@
 import styled, { css } from "styled-components";
 
 const Card = styled.div`
-overflow: hidden;
-padding: 0 0 32px;
-margin: 48px auto 0;
-width: 300px;
-color: #fff
-box-shadow: 0 0 20px rgba(0, 0, 0, .05), 0 0px 40px rgba(0, 0, 0, .08);
-border-radius: 5px;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+width: 40%;
+padding: 45px;
 `;
 
 export default Card;
