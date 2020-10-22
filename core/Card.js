d900c79c4a0da7694aee5ea8f480735c0910a745
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-22 18:02:08 -0300

    Voltando a versão

--- core/Card.js
+++ core/Card.js
@@ -1,10 +1,10 @@
-// import styled, { css } from "styled-components";
+import styled, { css } from "styled-components";
 
-// const Card = styled.div`
-// box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-// background-color: #fff;
-// padding-top: 30px;
-// padding-bottom: 30px;
-// `;
+const Card = styled.div`
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+background-color: #fff;
+padding-top: 30px;
+padding-bottom: 30px;
+`;
 
-// module.exports.Card = Card;
+export default Card;
